2dd575dbcb3ac66404837244e6efaa9968f0486d
Edit45 <poket72586@gamil.com>
2023-04-18 14:27:40 -0500

    he

--- hint.java
+++ hint.java
@@ -3,6 +3,16 @@ public class hint
 {
     public static void main(String[] args) throws Exception {
         test app = new test();  
-        System.out.println(app.selectMovie()); 
+         
+        int random = (int)(Math.random()*3)+1;
+        if(random == 1){
+            System.out.println(app.selectMovie());
+        }
+        else if(random == 2){
+            System.out.println(app.selectAnimals());
+        }
+        else{
+            System.out.println(app.selectAnimals());
+        }  
     }
 }
