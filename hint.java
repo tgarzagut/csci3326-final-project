682350e145d417514df572baa24488bc3fea97e6
Edit45 <poket72586@gamil.com>
2023-04-20 15:25:32 -0500

    all

--- hint.java
+++ hint.java
@@ -2,10 +2,7 @@ import finalPackage.*;
 public class hint 
 {
     public static void main(String[] args) throws Exception {
-        test app = new test();  
-        String s = "apple";
-        String f = "app  ";
-        char h = app.hint(s,f);
-        System.out.println(h);
+        test app = new test();
+        System.out.println(app.selectAnimals());
     }
 }
