9b73b91854e5e4197013791f0eea0eb8bbd70075
Edit45 <poket72586@gamil.com>
2023-04-24 15:57:26 -0500

    kermit

--- apr18/demo/src/main/java/com/example/Win.java
+++ apr18/demo/src/main/java/com/example/Win.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Win {
@@ -7,6 +11,20 @@ public class Win {
         Stage newStage = new Stage();
 
         newStage.initOwner(oldStage);
+
+        Label game_status = new Label("You win!");
+        game_status.setStyle("-fx-font-size: 36px");
+
+        Label congrats = new Label("Congratulations!");
+        congrats.setStyle("-fx-font-size: 36px");
+        
+        VBox finalBox = new VBox(congrats, game_status);
+        finalBox.setSpacing(30);
+        finalBox.setAlignment(Pos.CENTER);
+
+        Scene currentScene = new Scene(finalBox, 412, 284);
+        newStage.setScene(currentScene);
+        newStage.show();
         
     }
 }
